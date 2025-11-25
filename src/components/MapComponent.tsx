a658148b53cf525c37c3150aaae157d25ea21de2
Aram-1999 <aramtorosyan1999@gmail.com>
2025-11-25 15:23:58 -0800

    change the icon to default

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,5 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+import L from "leaflet";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+const defaultIcon = L.icon({
+  iconUrl,
+  iconRetinaUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
+
 interface MapComponentType {
   lat: number;
   lng: number;
